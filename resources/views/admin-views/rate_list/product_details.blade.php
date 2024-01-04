d158a33a4b1d96bdad5c169e42721eb6aa4c5d09
NimrahDqot <nimrah@dqotsolutions.com>
2024-01-04 12:08:13 +0530

    h

--- resources/views/admin-views/rate_list/product_details.blade.php
+++ resources/views/admin-views/rate_list/product_details.blade.php
@@ -16,7 +16,7 @@
                      <small>(Avg Rate)</small>
                  </th>
           
-                 <th width="24%">
+                 <th width="25%">
                      <div class="row">
                          <div class="row">
                              <div class="col-md-12">
@@ -58,8 +58,6 @@
                                  </div>
                              </div>
                          </div>
-
-
                      </div>
                  </th>
                  <th width="25%">
@@ -105,8 +103,6 @@
                                  </div>
                              </div>
                          </div>
-
-
                      </div>
                  </th>
                  <th width="25%">
@@ -130,7 +126,6 @@
                                   
                                  </div>
                              </div>
-
                              <div class="col-md-2">
                                  <div class="" style="margin-left: -25px;">
                                      <small for=""> Quantity:</small>
@@ -188,13 +183,8 @@
                  <td class="pt-1 pb-3 ">
                      <div class="row">
                          <input type="text" class="form-control 1_discount discount_<?php echo $key + 1; ?>" name=""   style="width: 70px;" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)"  >
-                         <input type="text" class="form-control 1_quantity quantity_<?php echo $key + 1; ?>" name="" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)"
-                             style="width: 70px;    margin: 0 2px;" value="1" >
-                         <!-- <select name="" class="form-control 1_quantity" style="width: 70px;    margin: 0 2px;">
-                                @for($i=1; $i<=100; $i++)
-                                    <option value="{{$i}}">{{$i}}</option>
-                                    @endfor
-                         </select> -->
+                         <input type="text" class="form-control 1_quantity quantity_<?php echo $key + 1; ?>" name="" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)"  style="width: 70px;    margin: 0 2px;" value="1" >
+                        
                          <select name="" class="form-control  1_unit"
                              style="width: 70px;    margin: 0 2px;    padding: 3px;">
                              <option value="">Unit</option>
@@ -202,15 +192,14 @@
                              <option value="{{$unit->id}}">{{$unit->title}}</option>
                              @endforeach
                          </select>
-                         <input type="text" class="form-control 1_offer_rate offer_rate_<?php echo $key + 1; ?>"
-                             name="discount[]" value="" style="width: 70px;">
+                         <input type="text" class="form-control 1_offer_rate offer_rate_<?php echo $key + 1; ?>" name="discount[]" value="" style="width: 70px;">
                      </div>
                  </td>
                  <td class="pt-1 pb-3 ">
                      <div class="row">
-                         <input type="text" class="form-control 2_discount discount_<?php echo $key + 1; ?>" onkeyup="handleFirstDiscountChange(1)" name="" style="width: 70px;">
+                         <input type="text" class="form-control 2_discount 2_discount_<?php echo $key + 1; ?>" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>,'2_')" name="" style="width: 70px;">
                         
-                         <input type="text" class="form-control 2_quantity quantity_<?php echo $key + 1; ?>"  name="" value="" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)"
+                         <input type="text" class="form-control 2_quantity 2_quantity_<?php echo $key + 1; ?>"  name="" value="" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>,'2_')"
                              style="width: 70px;    margin: 0 2px;">
                          <select name="" class="form-control  2_unit"
                              style="width: 70px;    margin: 0 2px;padding: 3px;">
@@ -219,15 +208,15 @@
                              <option value="{{$unit->id}}">{{$unit->title}}</option>
                              @endforeach
                          </select>
-                         <input type="text" class="form-control 2_offer_rate offer_rate_<?php echo $key + 1; ?>" name="discount[]" value=""
+                         <input type="text" class="form-control 2_offer_rate 2_offer_rate_<?php echo $key + 1; ?>" name="discount[]" value=""
                              style="width: 70px;">
                      </div>
                  </td>
                  <td class="pt-1 pb-3 ">
                      <div class="row">
-                         <input type="text" class="form-control 3_discount discount_<?php echo $key + 1; ?>" name="" style="width: 70px;" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)">
+                         <input type="text" class="form-control 3_discount 3_discount_<?php echo $key + 1; ?>" name="" style="width: 70px;" onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>,'3_')">
                          
-                         <input type="text" class="form-control 3_quantity quantity_<?php echo $key + 1; ?>" name="" value='' onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>)"
+                         <input type="text" class="form-control 3_quantity 3_quantity_<?php echo $key + 1; ?>" name="" value='' onkeyup="handleFirstDiscountChange(<?php echo $key + 1; ?>,'3_')"
                              style="width: 70px;    margin: 0 2px;">
                          <select name="" class="form-control 3_unit"
                              style="width: 70px;    margin: 0 2px;padding: 3px;">
@@ -236,7 +225,7 @@
                              <option value="{{$unit->id}}">{{$unit->title}}</option>
                              @endforeach
                          </select>
-                         <input type="text" class="form-control 3_offer_rate   offer_rate_<?php echo $key + 1; ?>" name="discount[]" value=""
+                         <input type="text" class="form-control 3_offer_rate   3_offer_rate_<?php echo $key + 1; ?>" name="discount[]" value=""
                              style="width: 70px;">
                      </div>
                  </td>
@@ -288,108 +277,122 @@
 //     // dynamicHandler(true);
 
 // });
-
-function handleFirstDiscountChange(id){
-    console.log("on change handleFirstDiscountChange")
-    console.log(id)
-
-// 1_offer_rate
-    let discount = parseFloat($(`.discount_${id}`).val())
-    // console.log("discount: ",discount)
-                const productRate = parseFloat($(`.product_rate_${id}`).text())
-
-                
-                let quantity = parseFloat($(`.quantity_${id}`).val());
-                
-                if (!quantity) {
-                    quantity = 1;
-                }
-                
-                let marginToBeAdd = ((quantity * productRate) * discount / 100)
-                let offerRate = (quantity * productRate) +marginToBeAdd ;
-                $(`.offer_rate_${id}`).val(offerRate.toFixed(2));
+    /**
+     * @function handleFirstDiscountChange
+     * ye dklglksldkgld
+     * @return  void
+     */
+    function handleFirstDiscountChange(id,classPrefix = ""){
 
 
-}
-function globalDynamicHandler(_item,i,value) {
-                let _sno = i + 1;
-                if (!_item.classList.contains("data_changed")) {
-                    _item.value = value;
-                }
-                // console.log("item: ", _item)
-                // 1_offer_rate
-    let discount = parseFloat($(`.discount_${_sno}`).val())
 
-                console.log("globalDynamicHandler discount: ", discount)
-                console.log("globalDynamicHandler discount_by_global: ", discount)
-                
-                
-                const productRate = parseFloat($(`.product_rate_${_sno}`).text())
-                
-                let quantity = parseFloat($(`.quantity_${_sno}`).val());
-                if (!quantity) {
-                    quantity = 1;
-                }
-                
-                let marginToBeAdd = ((quantity * productRate) * discount / 100)
-                let offerRate = (quantity * productRate) +marginToBeAdd ;
-                $(`.offer_rate_${_sno}`).val(offerRate.toFixed(2));
-            }
-var dynamicHandler = (update = false) => {
-    Array.from({
-        length: 3
-    }).forEach((item, index) => {
-        let sno = index + 1;
+        let discount = parseFloat($(`.${classPrefix}discount_${id}`).val())
 
-        let fixDiscount = parseFloat($("#discount").val() || 0)
-        let adminRate = parseFloat($(".offer_rate").val() || 0)
+        if (!discount) {
+            discount = 0;
+        }
+        const productRate = parseFloat($(`.product_rate_${id}`).text())
+        let quantity = parseFloat($(`.${classPrefix}quantity_${id}`).val());
+        if (!quantity) {
+            quantity = 1;
+        }
+        let marginToBeAdd = ((quantity * productRate) * discount / 100)
+        let offerRate = (quantity * productRate) +marginToBeAdd ;
+        $(`.${classPrefix}offer_rate_${id}`).val(offerRate.toFixed(2));
+    }
+/** @function  globalDynamicHandler
+ * step 2 
+ * @return void
+*/
+    function globalDynamicHandler(_item,i,value,columnPrefix = "") {
+        console.log('globalDynamicHandler', _item,i,value,columnPrefix);
+        let _sno = i + 1;
+        if (!_item.classList.contains("data_changed")) {
+            _item.value = value;
+        }
+        let discount = parseFloat($(`.${columnPrefix}discount_${_sno}`).val())
 
+        if (!discount) {
+            discount = 0;
+        }
+        const productRate = parseFloat($(`.product_rate_${_sno}`).text())
+
+        let quantity = parseFloat($(`.${columnPrefix}quantity_${_sno}`).val());
+        if (!quantity) {
+            quantity = 1;
+        }
+        let marginToBeAdd = ((quantity * productRate) * discount / 100)
+        let offerRate = (quantity * productRate) +marginToBeAdd ;
+        $(`.${columnPrefix}offer_rate_${_sno}`).val(offerRate.toFixed(2));
+    }
+    /**  @function dynamicHandler()
+     * step 1 start from here
+     * first its use for 3 times global <td> margin, quantity  
+     * we define array lenght 3 for <td> if we want to increase our th then change lenght
+     * index value is (0,1,2)
+     * we use sno for change index value (1,2,3)
+     * 
+     *    $(`#global_${sno}_discount`) this is use for first global discount its change all first <td> price value in this we pass
+     * global_1_discount and sno  value is 3 times( 3 1)(3 2)(3 3)
+     * then we change every product td value then we use this (${sno}_discount) in this we use loop every time 
+     * goto step 2
+     * return void
+     * 
+    */
+
+    var dynamicHandler = (update = false) => {
+        Array.from({
+            length: 3
+        }).forEach((item, index) => {
        
+                let sno = index + 1;
+                // let fixDiscount = parseFloat($("#discount").val() || 0)
+                // let adminRate = parseFloat($(".offer_rate").val() || 0)
 
-        /** @function first column's global discount, quantity & unit handler */
-        $(`#global_${sno}_discount`).on('input', function() {
-            var global_1_discount = $(this).val();
-            Array.from($(`.${sno}_discount`)).forEach((_item,i)=>globalDynamicHandler(_item,i,global_1_discount));
+                /** @function first column's global discount, quantity & unit handler */
+                $(`#global_${sno}_discount`).on('input', function() {
+                    var global_1_discount = $(this).val();
+                    Array.from($(`.${sno}_discount`)).forEach((item,i)=>globalDynamicHandler(item,i,global_1_discount, sno > 1 ? `${sno}_`:""));
 
-        });
+                });
 
-        $(`#global_${sno}_quantity`).on('input', function() {
-            var global_1_quantity = $(this).val();
-            // console.log(global_1_quantity);
-            Array.from($(`.${sno}_quantity`)).forEach((_item,i)=>globalDynamicHandler(_item,i,global_1_quantity));
+                $(`#global_${sno}_quantity`).on('input', function() {
+                    var global_1_quantity = $(this).val();
+                    // console.log(global_1_quantity);
+                    Array.from($(`.${sno}_quantity`)).forEach((_item,i)=>globalDynamicHandler(_item,i,global_1_quantity, sno > 1 ? `${sno}_`:""));
 
-        });
-        $(`#global_${sno}_unit`).on('change', function() {
-            var global_1_unit = $(this).val();
-            // console.log(global_1_unit);
-            Array.from($(`.${sno}_unit`)).forEach(function(_item) {
-                if (!_item.classList.contains("data_changed")) {
-                    _item.value = global_1_unit;
-                }
-            });
+                });
+                $(`#global_${sno}_unit`).on('change', function() {
+                    var global_1_unit = $(this).val();
+                    // console.log(global_1_unit);
+                    Array.from($(`.${sno}_unit`)).forEach(function(_item) {
+                        if (!_item.classList.contains("data_changed")) {
+                            _item.value = global_1_unit;
+                        }
+                    });
 
-        });
-        /** @function first column's global discount, quantity & unit handler */
+                });
+                /** @function first column's global discount, quantity & unit handler */
 
-        /** @function first column's discount, quantity & unit handler */
-        Array.from($(`.${sno}_discount`)).forEach(function(_item) {
-            _item.oninput = function(event) {
-                event.target.classList.add("data_changed");
-            }
-        });
-        Array.from($(`.${sno}_quantity`)).forEach(function(_item) {
-            _item.oninput = function(event) {
-                event.target.classList.add("data_changed");
-            }
-        });
-        Array.from($(`.${sno}_unit`)).forEach(function(_item) {
-            _item.oninput = function(event) {
-                event.target.classList.add("data_changed");
-            }
-        });
-        /** @function first column's discount, quantity & unit handler */
+                /** @function first column's discount, quantity & unit handler */
+                Array.from($(`.${sno}_discount`)).forEach(function(_item) {
+                    _item.oninput = function(event) {
+                        event.target.classList.add("data_changed");
+                    }
+                });
+                Array.from($(`.${sno}_quantity`)).forEach(function(_item) {
+                    _item.oninput = function(event) {
+                        event.target.classList.add("data_changed");
+                    }
+                });
+                Array.from($(`.${sno}_unit`)).forEach(function(_item) {
+                    _item.oninput = function(event) {
+                        event.target.classList.add("data_changed");
+                    }
+                });
+                /** @function first column's discount, quantity & unit handler */
     })
-}
+    }
 
 dynamicHandler()
  </script>

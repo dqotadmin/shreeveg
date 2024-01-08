ec67a362983d5a0536de62e573ba3bf020f45aff
mukeshdqot <aslam@dqotsolutions.com>
2024-01-08 19:02:46 +0530

    changes

--- app/Http/Controllers/Admin/DonationController.php
+++ app/Http/Controllers/Admin/DonationController.php
@@ -74,13 +74,6 @@ class DonationController extends Controller
         return view($this->view_folder . '.index', compact('rows', 'search', 'user'));
     }
 
-    public function destroy($id)
-    {
-        $this->mTable::find($id)->delete();
-        Toastr::success(translate('order remved'));
-        return back();
-    }
-
     public function create(Request $request)
     {
         $store_id = '';
@@ -100,7 +93,7 @@ class DonationController extends Controller
     {
         //dd($request->all());
         try {
-            //DB::beginTransaction();
+            DB::beginTransaction();
             $request->validate([
                 'product_id/*' => 'required',
                 'qty/*' => 'required'
@@ -111,7 +104,7 @@ class DonationController extends Controller
             if ($request->product_id) {
                 foreach ($request->product_id as $key => $productId) {
                     if (isset($request->qty[$key])) {
-                        dump($productId, $request->warehouse_id);
+                        // dump($productId, $request->warehouse_id);
                         $row = new $this->mTable;
 
                         $row->product_id = $productId;
@@ -133,99 +126,9 @@ class DonationController extends Controller
             }
 
 
-            //DB::commit();
+            DB::commit();
             Toastr::success(translate('data Inserted Successfully!'));
             return redirect()->route('admin.product.donations.index');
-        } catch (\Exception $e) {
-            // DB::rollback();
-            $msg = $e->getMessage();
-            dd($msg);
-            \Session::flash('warning', $msg);
-            return redirect()->back()->withInput();
-        }
-    }
-    public function show(Request $request, $id)
-    {
-        $user = auth('admin')->user();
-        $row = $this->mTable::find($id);
-        return view($this->view_folder . '.show', compact('row', 'user'));
-    }
-    public function stockUpdate(Request $request, $id)
-    {
-        $row = $this->mTable::find($id);
-
-        if ($row->status == 'Received') {
-            $row->stock_update = 'Stock Update';
-
-            $row->save();
-
-            $storeProducts = \App\Model\PurchaseStoreOrderDetail::where('purchase_store_order_id', $row->id)->get();
-            $store_id = $row->store_id;
-            // dd($storeProducts,'in if');
-            if ($storeProducts->isNotEmpty()) {
-                foreach ($storeProducts as $storeProduct) {
-                    $total_stock = $storeProduct->qty;
-                    $product_id = $storeProduct->product_id;
-
-                    $purchaseStoreOrderDetail = \App\Model\StoreProduct::firstOrNew([
-                        'store_id' => $store_id,
-                        'product_id' => $product_id,
-                    ]);
-
-                    // Set or update the properties
-                    $purchaseStoreOrderDetail->total_stock = ($purchaseStoreOrderDetail->total_stock ?? 0) + $total_stock;
-                    $purchaseStoreOrderDetail->status = '1'; // Set the status if needed
-
-                    // Save the record (insert if it's a new record, update if it already exists)
-                    $purchaseStoreOrderDetail->save();
-                }
-            }
-        }
-
-
-        Toastr::success(translate('Stock updated Successfully!'));
-        return redirect()->route('admin.store.purchase-store-orders.index');
-    }
-    public function updateStatus(Request $request, $id)
-    {
-
-        try {
-            DB::beginTransaction();
-            $role = auth('admin')->user()->admin_role_id;
-            $user = auth('admin')->user();
-            $row = $this->mTable::find($id);
-            if ($request->status == 'Received' && $row->status != 'Delivered') {
-                Toastr::error(translate('order not delivered till now!'));
-                return redirect()->back()->withInput();
-            }
-
-            $row->status = $request->status;
-            if ($role == 6) {
-                $row->store_comments = $request->store_comments;
-            } else {
-                $row->warehouse_comments = $request->warehouse_comments;
-            }
-
-            if ($request->status == 'Delivered') {
-                foreach ($request->product_id as $key => $productId) {
-                    if (isset($request->qty[$key])) {
-
-                        $whProductRow =   \App\Model\WarehouseProduct::where('warehouse_id', $user->warehouse_id)->where('product_id', $productId)->first();
-                        if ($whProductRow->total_stock > $request->qty[$key]) {
-                            $whProductRow->decrement('total_stock', $request->qty[$key]);
-                        } else {
-                            //dd($whProductRow->productDetail->name);
-                            Toastr::error(translate('stock not availale for ' . $whProductRow->productDetail->name));
-                            return redirect()->back();
-                        }
-                    }
-                }
-            }
-
-            $row->save();
-            DB::commit();
-            Toastr::success(translate('status updated Successfully!'));
-            return redirect()->route('admin.store.purchase-store-orders.index');
         } catch (\Exception $e) {
             DB::rollback();
             $msg = $e->getMessage();

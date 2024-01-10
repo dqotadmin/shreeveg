aba71af9a43b16a3eeb618bab20ed9749cfb2542
mukeshdqot <aslam@dqotsolutions.com>
2024-01-10 10:51:52 +0530

    dd remove

--- app/Http/Controllers/Admin/PurchaseStoreOrderController.php
+++ app/Http/Controllers/Admin/PurchaseStoreOrderController.php
@@ -39,7 +39,6 @@ class PurchaseStoreOrderController extends Controller
         $this->store_product = '\App\Model\StoreProduct';
         $this->warehouse_product = '\App\Model\WarehouseProduct';
         $this->user = auth('admin')->user();
-
     }
 
     /**
@@ -90,20 +89,20 @@ class PurchaseStoreOrderController extends Controller
     {
 
         $user = auth('admin')->user();
-        if(($user->store_id) > 0){
+        if (($user->store_id) > 0) {
             $wahrehouseId = $user->Store->warehouse_id;
-        }else{
+        } else {
             $wahrehouseId = $user->warehouse_id;
         }
         $categories = \App\Model\WarehouseCategory::where('warehouse_id', $wahrehouseId)->whereStatus('1')->get();
-        return view($this->view_folder . '.create', compact('categories', 'wahrehouseId','user'));
+        return view($this->view_folder . '.create', compact('categories', 'wahrehouseId', 'user'));
     }
 
     public function store(Request $request)
     {
         try {
             //DB::beginTransaction();
-            $request->validate([ 
+            $request->validate([
                 'product_id/*' => 'required',
                 'rate/*' => 'required',
                 'available_qty/*' => 'required'
@@ -120,7 +119,7 @@ class PurchaseStoreOrderController extends Controller
             $row->store_comments = $request->store_comments;
 
             if ($row->save()) {
-              
+
                 $finalPrice = 0;
                 if ($request->product_id) {
                     foreach ($request->product_id as $key => $productId) {
@@ -137,18 +136,18 @@ class PurchaseStoreOrderController extends Controller
                             $tableDetail->save();
                             $finalPrice += $totalPrice;
                             //code for store stock 
-                        //     $store_id = $row->id;
-                        //     $storeProduct = \App\Model\StoreProduct::where('store_id', $store_id)->where('product_id', $productId)->get();
-                        //     if(empty($store_product)){
-                        //         $storeProduct->firstOrNew();
-                        //     }else{
-                        //     $storeProduct->increment('total_stock',$request->qty[$key]);
-                        //     }
-                        //     // Update the total_stock column
-                        // // Save the changes
-                        // $storeProduct->save();
-                        //     if($store_id && $productId)
-                          
+                            //     $store_id = $row->id;
+                            //     $storeProduct = \App\Model\StoreProduct::where('store_id', $store_id)->where('product_id', $productId)->get();
+                            //     if(empty($store_product)){
+                            //         $storeProduct->firstOrNew();
+                            //     }else{
+                            //     $storeProduct->increment('total_stock',$request->qty[$key]);
+                            //     }
+                            //     // Update the total_stock column
+                            // // Save the changes
+                            // $storeProduct->save();
+                            //     if($store_id && $productId)
+
                             // if (isset($request->qty[$key])) {
                             //     $store_product = new \App\Model\StoreProduct();
                             //     $store_product['store_id'] = $user->store_id;
@@ -170,7 +169,7 @@ class PurchaseStoreOrderController extends Controller
         } catch (\Exception $e) {
             // DB::rollback();
             $msg = $e->getMessage();
-            dd($msg);
+            //dd($msg);
             \Session::flash('warning', $msg);
             return redirect()->back()->withInput();
         }
@@ -181,44 +180,45 @@ class PurchaseStoreOrderController extends Controller
         $row = $this->mTable::find($id);
         return view($this->view_folder . '.show', compact('row', 'user'));
     }
-    public function stockUpdate(Request $request, $id) {
+    public function stockUpdate(Request $request, $id)
+    {
         $row = $this->mTable::find($id);
-       
-        if($row->status == 'Received'){
+
+        if ($row->status == 'Received') {
             $row->stock_update = 'Stock Update';
-        
+
             $row->save();
-       
+
             $storeProducts = \App\Model\PurchaseStoreOrderDetail::where('purchase_store_order_id', $row->id)->get();
             $store_id = $row->store_id;
-       // dd($storeProducts,'in if');
-        if ($storeProducts->isNotEmpty()) {
-        foreach ($storeProducts as $storeProduct) {
+            // dd($storeProducts,'in if');
+            if ($storeProducts->isNotEmpty()) {
+                foreach ($storeProducts as $storeProduct) {
                     $total_stock = $storeProduct->qty;
                     $product_id = $storeProduct->product_id;
-                    
+
                     $purchaseStoreOrderDetail = \App\Model\StoreProduct::firstOrNew([
                         'store_id' => $store_id,
                         'product_id' => $product_id,
                     ]);
-        
+
                     // Set or update the properties
                     $purchaseStoreOrderDetail->total_stock = ($purchaseStoreOrderDetail->total_stock ?? 0) + $total_stock;
                     $purchaseStoreOrderDetail->status = '1'; // Set the status if needed
-        
+
                     // Save the record (insert if it's a new record, update if it already exists)
                     $purchaseStoreOrderDetail->save();
                 }
             }
         }
 
-        
+
         Toastr::success(translate('Stock updated Successfully!'));
         return redirect()->route('admin.store.purchase-store-orders.index');
     }
     public function updateStatus(Request $request, $id)
     {
-       
+
         try {
             DB::beginTransaction();
             $role = auth('admin')->user()->admin_role_id;
@@ -235,25 +235,23 @@ class PurchaseStoreOrderController extends Controller
             } else {
                 $row->warehouse_comments = $request->warehouse_comments;
             }
-           
-            if($request->status == 'Delivered'){
-                foreach($request->product_id as $key => $productId){
-                    if(isset($request->qty[$key])){
-                    
-                        $whProductRow =   \App\Model\WarehouseProduct::where('warehouse_id',$user->warehouse_id)->where('product_id',$productId)->first();
-                        if($whProductRow->total_stock > $request->qty[$key]){
+
+            if ($request->status == 'Delivered') {
+                foreach ($request->product_id as $key => $productId) {
+                    if (isset($request->qty[$key])) {
+
+                        $whProductRow =   \App\Model\WarehouseProduct::where('warehouse_id', $user->warehouse_id)->where('product_id', $productId)->first();
+                        if ($whProductRow->total_stock > $request->qty[$key]) {
                             $whProductRow->decrement('total_stock', $request->qty[$key]);
-                        }else{
+                        } else {
                             //dd($whProductRow->productDetail->name);
-                            Toastr::error(translate('stock not availale for '.$whProductRow->productDetail->name));
+                            Toastr::error(translate('stock not availale for ' . $whProductRow->productDetail->name));
                             return redirect()->back();
                         }
                     }
-        
                 }
-         
             }
-           
+
             $row->save();
             DB::commit();
             Toastr::success(translate('status updated Successfully!'));
